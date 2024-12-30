e36a7d300a544164f90bd3f8c71dfc3bf888eba1
Gayathri Mahadevan <gayat_19@yahoo.com>
2024-12-30 16:13:36 +0530

    Before merge

--- obj/Debug/net8.0/SampleAnanlysisAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/SampleAnanlysisAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SampleAnanlysisAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+990e3f57669b2068f1e9798ded7c757b92c4990d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SampleAnanlysisAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SampleAnanlysisAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
